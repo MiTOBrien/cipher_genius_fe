1d7e713ea47e84de47589ef84ab19c27bf92bd12
Michael OBrien <obrien.michael.t@gmail.com>
2025-04-16 16:16:53 -0400

    adds info and styling to the about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1 class="header">History of Ciphers</h1>
+    <p>
+      Classical ciphers, or cryptography, has existed for thousands of years, and it wasn't until
+      the early 20th century that more complex mechanical and electromechanical machines such as the
+      Enigma rotor machine were invented and advanced crytography as we know it.
+    </p>
+    <h2 class="header">Caesar Cipher</h2>
+    <p>
+      The Caesar cipher is a simple substitution cipher where letters are shifted and subsituted to
+      encrypt the message and was used by Julius Caesar to protect messages of military
+      significance. He supposedly used a standard shift of 3 letters for his communications and his
+      new nephew, Augustus, also used this cipher method with a standard shift of 1 letter.
+    </p>
+    <p>More cipher history and information coming soon.</p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+p {
+  color: #84ce00;
+}
+
+.header {
+  color: #01bfff;
 }
 </style>
