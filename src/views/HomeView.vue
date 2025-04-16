da90481516e70f91bd273d8b23d2248f4c724900
Michael OBrien <obrien.michael.t@gmail.com>
2025-04-16 13:43:07 -0400

    updates font color on homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,12 @@
 
 <template>
   <p>What is a cipher?</p>
+  <p>A cipher is a method used for performing encryption or decryption. Essentially, it's a set of algorithms that you apply to your data (plaintext) to transform it into an unrecognizable form (ciphertext).</p>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+p {
+  margin-left: 20px;
+  color: #84ce00;
 }
 </style>
