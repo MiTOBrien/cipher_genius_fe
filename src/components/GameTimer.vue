93c738f4affed1d87f4a746e639203068eaa1914
Michael OBrien <obrien.michael.t@gmail.com>
2025-05-06 09:21:06 -0400

    adds a timer to the ciphers pages

---
+++ src/components/GameTimer.vue
@@ -0,0 +1,54 @@
+<!-- src/components/Timer.vue -->
+<template>
+  <div class="timer">{{ formattedTime }}</div>
+</template>
+
+<script setup>
+import { ref, computed, watch, onUnmounted } from 'vue'
+
+const props = defineProps({
+  isRunning: Boolean, // parent controls when it starts/stops
+  resetTrigger: Number, // force reset on change (can be a timestamp)
+})
+
+const seconds = ref(0)
+let intervalId = null
+
+const formattedTime = computed(() => {
+  const mins = String(Math.floor(seconds.value / 60)).padStart(2, '0')
+  const secs = String(seconds.value % 60).padStart(2, '0')
+  return `${mins}:${secs}`
+})
+
+watch(
+  () => props.isRunning,
+  (running) => {
+    if (running) {
+      intervalId = setInterval(() => {
+        seconds.value++
+      }, 1000)
+    } else {
+      clearInterval(intervalId)
+    }
+  },
+)
+
+watch(
+  () => props.resetTrigger,
+  () => {
+    seconds.value = 0
+    clearInterval(intervalId)
+  },
+)
+
+onUnmounted(() => clearInterval(intervalId))
+</script>
+
+<style scoped>
+.timer {
+  font-size: 1.5rem;
+  color: #84ce00;
+  text-align: center;
+  margin-top: 1rem;
+}
+</style>
